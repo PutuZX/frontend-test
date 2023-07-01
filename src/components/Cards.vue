a3c5577e85dfbbf91d5c490cf70aa9fb09c54dda
PutuZX <putubaguswidia@gmail.com>
2023-07-01 22:36:15 +0800

    make the homepage to be responsive

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,12 +1,53 @@
 <template>
-
+  <div v-for="card in cards" :key="card.id">
+    <div
+      v-if="card.enabled == true"
+      class="flex flex-col items-center justify-center bg-white pt-8 pb-8 w-[400px] h-[250px] m-3 rounded-md border space-y-5 cursor-pointer"
+    >
+      <Play size="70" v-if="card.icon == 'play'" />
+      <Message size="70" v-else-if="card.icon == 'comment'" />
+      <Monitor size="70" v-else-if="card.icon == 'deskop'" />
+      <Link size="70" v-else-if="card.icon == 'link'" />
+      <Gem size="70" v-else-if="card.icon == 'gem'" />
+      <Cart size="70" v-else />
+      <p class="font-bold text-xl">{{ card.title }}</p>
+      <div class="text-center">
+        <p class="text-[#03A84E]">{{ card.totalArticle }} articles</p>
+        <p class="text-xs text-[#9C9AA6]">{{ formatDate(card.updatedOn) }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Monitor from "./icon/Monitor.vue";
+import Play from "./icon/Play.vue";
+import Message from "./icon/Message.vue";
+import Gem from "./icon/Gem.vue";
+import Cart from "./icon/Cart.vue";
+import Link from "./icon/Link.vue";
+import { useCards } from "../store";
+import { formatDistanceToNow } from "date-fns";
+import { toRef } from "vue";
 
 export default {
-    setup(){
-        
+  setup() {
+    const store = useCards();
+    function formatDate(date) {
+      return "Last update " + formatDistanceToNow(new Date(date)) + " ago";
     }
-}
+    return {
+      cards: toRef(store, "cards"),
+      formatDate,
+    };
+  },
+  components: {
+    Monitor,
+    Play,
+    Message,
+    Gem,
+    Cart,
+    Link,
+  },
+};
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,35 +5,20 @@
       <h3>Everything you need to manage your messaging</h3>
     </div>
     <div class="flex items-center justify-center pt-6">
-      <input class="border border p-4 w-[600px] focus:outline-none rounded-l-md" placeholder="Search for answers" />
+      <input class="border border p-4 w-[600px] focus:outline-none rounded-l-md ml-8" placeholder="Search for answers" />
 
-      <div class="bg-[#03A84E] p-4 border-[#03A84E] border rounded-r-md w-[65px] text-center cursor-pointer">
+      <div class="bg-[#03A84E] p-4 border-[#03A84E] border rounded-r-md w-[65px] text-center cursor-pointer mr-8">
 		<i class="fa fa-search fa-lg text-white"></i>
 	  </div>
     </div>
   </div>
 </template>
 
-<script>
-import {reactive} from "vue"
-
-export default {
-	setup(){
-		const test = reactive("test");
-		console.log(test);
-
-		return{
-			test
-		}
-	}
-};
-</script>
-
 <style lang="scss" scoped>
 @import "../scss/_variables.scss";
 
 #header-wrapper {
-  height: 330px; /* replace this with your actual header height */
+  min-height: 330px; /* replace this with your actual header height */
 }
 
 #header-text {
